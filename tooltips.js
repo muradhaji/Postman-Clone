47c609825a399e93a80770cc99403eadb5c2eee6
muradhaji <muradhajiyev42@icloud.com>
2025-06-25 12:10:23 +0400

    First commit, header and footer created

---
+++ tooltips.js
@@ -0,0 +1,13 @@
+tippy('.hideSidebarButton', {
+    content: 'Hide Sidebar (CTRL + /)',
+    placement: 'top',
+    animation: 'fade',
+    theme: 'tooltipTheme'
+});
+
+tippy('.layoutButton', {
+    content: 'Two pane view (CTRL + ALT + V)',
+    placement: 'top',
+    animation: 'fade',
+    theme: 'tooltipTheme'
+});
